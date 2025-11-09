d27313be90d58e9d93ecd235b9adbe5027cb8680
giri-daran <114486252+giri-daran@users.noreply.github.com>
2025-11-09 12:29:26 +0530

    Add files via upload

---
+++ Episerver-sol/Models/SiteContentType.cs
@@ -0,0 +1,13 @@
+namespace EpiserverBH.Models
+{
+    ///<summary>
+    /// Attribute used for site content types to set default attribute values
+    ///</summary>
+    public class SiteContentType : ContentTypeAttribute
+    {
+        public SiteContentType()
+        {
+            GroupName = Globals.GroupNames.Default;
+        }
+    }
+}
